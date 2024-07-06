f541831f97cd28281d700efcd242d2bd3a632686
Daniel B <daarf92@gmail.com>
2024-07-06 19:26:40 +0100

    users table added to db and create user request added

--- db/db.go
+++ db/db.go
@@ -19,7 +19,24 @@ func InitDB() {
 	DB.SetMaxOpenConns(10)
 	DB.SetMaxIdleConns(5)
 
+	createUsers()
 	createTables()
+}
+
+func createUsers() {
+	createUsersTable := `
+	CREATE TABLE IF NOT EXISTS users (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		email TEXT NOT NULL UNIQUE,
+		password TEXT NOT NULL
+	)
+	`
+
+	_, err := DB.Exec(createUsersTable)
+
+	if err != nil {
+		panic("Could not create users table")
+	}
 }
 
 func createTables() {
@@ -30,11 +47,10 @@ func createTables() {
 		description TEXT NOT NULL,
 		location TEXT NOT NULL,
 		dateTime DATETIME NOT NULL,
-		user_id INTEGER
-
+		user_id INTEGER,
+		FOREIGN KEY(user_id) REFERENCES users(id)
 	)
 	`
-
 	_, err := DB.Exec(createEventsTable)
 
 	if err != nil {
